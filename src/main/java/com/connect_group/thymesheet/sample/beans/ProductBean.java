3b255645371db00fe79b02aca9dd1b9eb45080f8
Adam Perry <aperry7@gmail.com>
2013-10-17 15:20:38 +0100

    Added table example from the Thymeleaf homepage

---
+++ src/main/java/com/connect_group/thymesheet/sample/beans/ProductBean.java
@@ -0,0 +1,31 @@
+package com.connect_group.thymesheet.sample.beans;
+
+public class ProductBean {
+
+	private String name;
+	private double price;
+	
+	public ProductBean() {}
+	
+	public ProductBean(String name, double price) {
+		this.name = name;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+}
--- src/main/java/com/connect_group/thymesheet/sample/mvc/HomePageController.java
+++ src/main/java/com/connect_group/thymesheet/sample/mvc/HomePageController.java
@@ -1,6 +1,7 @@
 package com.connect_group.thymesheet.sample.mvc;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.connect_group.thymesheet.sample.beans.LinkBean;
+import com.connect_group.thymesheet.sample.beans.ProductBean;
 
 @Controller
 public class HomePageController {
@@ -25,6 +27,16 @@ public class HomePageController {
 		links.add(new LinkBean("http://www.thymeleaf.org/", "Thymeleaf Homepage"));
 		links.add(new LinkBean("http://www.connect-group.com/", "Connect Group Homepage"));
 		return links;
+	}
+	
+	@ModelAttribute("allProducts")
+	public List<ProductBean> getAllProducts() {
 		
+		ProductBean p1 = new ProductBean("Pen", 1.00);
+		ProductBean p2 = new ProductBean("Pencil", 0.50);
+		ProductBean p3 = new ProductBean("Paper", 0.02);
+		ProductBean p4 = new ProductBean("Note Book", 2.00);
+		
+		return Arrays.asList(p1,p2,p3,p4);
 	}
 }
